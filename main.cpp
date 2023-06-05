08de7fb2ca15c022be24a2b9cb8db8ec4ece52fa
Liplum <Li_plum@outlook.com>
2023-06-05 10:31:13 +0800

    added `nlohmann/json`

--- main.cpp
+++ main.cpp
@@ -1,6 +1,32 @@
 #include <iostream>
+#include <nlohmann/json.hpp>
 
 int main() {
-  std::cout << "Hello, World!" << std::endl;
+  // Create a JSON object
+  nlohmann::json data;
+
+  // Add key-value pairs to the JSON object
+  data["name"] = "John Doe";
+  data["age"] = 25;
+  data["city"] = "New York";
+
+  // Convert the JSON object to a string
+  std::string jsonStr = data.dump();
+
+  // Print the JSON string
+  std::cout << "JSON Data: " << jsonStr << std::endl;
+
+  // Parse a JSON string
+  std::string jsonData = R"({"status": "success", "code": 200})";
+  nlohmann::json parsedData = nlohmann::json::parse(jsonData);
+
+  // Access values from the parsed JSON
+  std::string status = parsedData["status"];
+  int code = parsedData["code"];
+
+  // Print the parsed values
+  std::cout << "Status: " << status << std::endl;
+  std::cout << "Code: " << code << std::endl;
+
   return 0;
 }
