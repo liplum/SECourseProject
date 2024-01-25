b5dbd114df4c7aa76cead5757e63788407b2fbd8
Liplum <Li_plum@outlook.com>
2024-01-25 16:44:04 +0800

    safely extract shared parts

--- prod-mgmt/src/auth.h
+++ prod-mgmt/src/auth.h
@@ -1,31 +1,77 @@
 //
-// Created by Liplum on 1/25/24.
+// Created by Liplum on 6/6/23.
 //
 
 #ifndef PRODMGMT_AUTH_H
 #define PRODMGMT_AUTH_H
 
-#include "../../shared/auth.h"
+#include <utility>
+#include <vector>
+#include "string"
+#include "../../shared/dirty.h"
+#include "nlohmann/json.hpp"
+
+using namespace std;
+using json = nlohmann::json;
 
 // User type with hardcoded permissions
 struct PermissionSet {
-    bool manageProduct = false;
-    bool manageUser = false;
+  bool manageProduct = false;
+  bool manageUser = false;
 
-    explicit PermissionSet(
-            bool modifyProduct = false,
-            bool modifyUser = false
-    ) : manageProduct(modifyProduct),
-        manageUser(modifyUser) {}
+  explicit PermissionSet(
+    bool modifyProduct = false,
+    bool modifyUser = false
+  ) : manageProduct(modifyProduct),
+      manageUser(modifyUser) {}
 
-    explicit PermissionSet(const json &obj);
+  explicit PermissionSet(const json &obj);
 
-    static PermissionSet all();
+  static PermissionSet all();
 
-    json toJson() const;
+  json toJson() const;
 };
 
-using KUser = User<PermissionSet>;
-using KAuth = Auth<PermissionSet>;
+struct User {
+  string account;
+  string password;
+  PermissionSet permission;
+
+  User() = default;
+
+  explicit User(
+    string account,
+    string password,
+    const PermissionSet &permission
+  ) : account(std::move(account)),
+      password(std::move(password)),
+      permission(permission) {
+  }
+
+  explicit User(const json &obj);
+
+  json toJson() const;
+};
+
+class Auth : public DirtyMarkMixin {
+private:
+  vector<User> users;
+public:
+  Auth() = default;
+
+  explicit Auth(const string &filename);
+
+  optional<User> findUserByAccount(const string &account);
+
+  bool addUser(const string &account, const string &password, const PermissionSet &permission);
+
+  bool removeUserByAccount(const string &account);
+
+  bool updateUser(User &user);
+
+  bool saveToFile(const string &filename);
+
+  vector<User> getUsers();
+};
 
 #endif //PRODMGMT_AUTH_H
